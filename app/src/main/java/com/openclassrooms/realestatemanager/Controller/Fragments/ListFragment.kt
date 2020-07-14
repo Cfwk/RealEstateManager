5e782d392315237c6465d32fd71459f80e905edc
Sindrosa <skiti1996@live.fr>
2020-07-14 20:23:56 +0200

    Reupload after extension of the Openclassrooms path

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/Controller/Fragments/ListFragment.kt
@@ -0,0 +1,117 @@
+package com.openclassrooms.realestatemanager.Controller.Fragments
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.openclassrooms.realestatemanager.Controller.Activities.MainActivity
+import com.openclassrooms.realestatemanager.Controller.ViewModel.EstateViewModel
+import com.openclassrooms.realestatemanager.Controller.Views.FragmentListAdapter
+import com.openclassrooms.realestatemanager.Di.Injection
+import com.openclassrooms.realestatemanager.Models.FullEstate
+import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.Utils.DividerItemDecoration
+import com.openclassrooms.realestatemanager.Utils.ItemClickSupport
+import kotlinx.android.synthetic.main.fragment_list.*
+
+
+const val DATABASE_ID = "DATABASE_ID"
+
+class ListFragment : Fragment() {
+
+    private lateinit var listEstate:ArrayList<FullEstate>
+    private lateinit var adapter:FragmentListAdapter
+    private lateinit var mViewModel: EstateViewModel
+
+    companion object {
+        fun newInstance(): ListFragment {
+            return ListFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        Log.e("ListFragment", "Displaying fragment...")
+        mViewModel = ViewModelProviders.of(this,Injection.provideViewModelFactory(this.context!!)).get(EstateViewModel::class.java)
+
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bundle = arguments?.size()
+        if (bundle != null) {
+            val query = arguments?.getString("QUERY")
+            val args = arguments?.getStringArrayList("ARGS") as ArrayList<Any>
+            mViewModel.getEstatesBySearch(query!!,args).observe(viewLifecycleOwner, Observer { updateUI(it!!) })
+        }else{
+            mViewModel.getEstates().observe(viewLifecycleOwner, Observer<List<FullEstate>> { updateUI(it!!) })
+        }
+
+        this.configureRecyclerView()
+        this.configureOnClickRecyclerView()
+    }
+
+    // ---------------------
+    // CONFIGURATION
+    // ---------------------
+    private fun configureRecyclerView(){
+        this.listEstate = ArrayList()
+        this.adapter = FragmentListAdapter(this.listEstate)
+        this.fragment_list_recycler_view.adapter = this.adapter
+        this.fragment_list_recycler_view.layoutManager = LinearLayoutManager(activity)
+        fragment_list_recycler_view.addItemDecoration(DividerItemDecoration(activity as Activity,0, 0))
+    }
+
+    private fun configureOnClickRecyclerView(){
+        ItemClickSupport.addTo(fragment_list_recycler_view, R.layout.fragment_list_item)
+                .setOnItemClickListener{recyclerView, position, v ->
+                    if ((activity as MainActivity).isTablet()){
+                        adapter.setBackgroundColorItemClicked(position)
+                    }
+                    this.launchDetailFragment(adapter.getEstateInfos(position).estate.id)
+        }
+    }
+
+    // ---------------------
+    // ACTION
+    // ---------------------
+
+    private fun launchDetailFragment(databaseId : Long){
+        val bundle = Bundle()
+        val newFragment = DetailFragment.newInstance()
+        bundle.putLong(DATABASE_ID,databaseId)
+        newFragment.arguments = bundle
+
+        val transaction = activity!!.supportFragmentManager.beginTransaction()
+
+        if((activity as MainActivity).isTablet()){
+            transaction.replace(R.id.fragment_view_detail, newFragment)
+        }else{
+            transaction.replace(R.id.fragment_view, newFragment)
+        }
+
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+    }
+
+    // ---------------------
+    // UI
+    // ---------------------
+
+    private fun updateUI(results:List<FullEstate>){
+        //results.forEach{ Log.e("LIST_UPDATE_UI","Estate Type/ID : ${it.estate.estateType}"+ " / ${it.estate.id} : " + it.images.toString())}
+        this.listEstate.clear()
+        this.listEstate.addAll(results)
+        adapter.notifyDataSetChanged()
+    }
+
+}
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/Controller/Fragments/LoanFragment.kt
@@ -0,0 +1,105 @@
+package com.openclassrooms.realestatemanager.Controller.Fragments
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.openclassrooms.realestatemanager.R
+import kotlinx.android.synthetic.main.fragment_loan.*
+
+
+class LoanFragment : Fragment() {
+
+    companion object {
+        fun newInstance() : LoanFragment{
+            return  LoanFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_loan, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        this.setOnClickListener()
+    }
+
+    // ---------------------
+    // CONFIGURATION
+    // ---------------------
+
+    private fun setOnClickListener(){
+        loan_term.setOnClickListener{displayPopupMenu()}
+        loan_calculate.setOnClickListener { calculate() }
+    }
+
+    private fun displayPopupMenu(){
+        val popupMenu = PopupMenu(this.context, loan_term)
+        popupMenu.menuInflater.inflate(R.menu.popup_menu_loan_term, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { item -> loan_term.setText(item.title); true}
+        popupMenu.show()
+    }
+
+    // ---------------------
+    // ACTION
+    // ---------------------
+
+    private fun calculate(){
+        var canCalculate = false
+        val amount = loan_amount.text.toString().toDoubleOrNull() ?: 0.0
+        val downPayment = loan_down.text.toString().toDoubleOrNull() ?: 0.0
+        val term = loan_term.text.toString().toDoubleOrNull()
+        val interest = loan_interest.text.toString().toDoubleOrNull()
+
+        when{
+            loan_amount.text.isNullOrEmpty() || loan_term.text.isNullOrEmpty() || loan_interest.text.isNullOrEmpty() || downPayment >= amount!! -> {
+                canCalculate = false
+                if (loan_amount.text.isNullOrEmpty()){
+                    loan_amount_layout.error = resources.getString(R.string.loan_error)
+                }
+                if (loan_term.text.isNullOrEmpty()){
+                    loan_term_layout.error = resources.getString(R.string.loan_error)
+                }
+                if (loan_interest.text.isNullOrEmpty()){
+                    loan_interest_layout.error = resources.getString(R.string.loan_error)
+                }else if (interest!! < 0 || interest > 100){
+                    loan_interest_layout.error = resources.getString(R.string.loan_error_interest_value)
+                }
+                if (downPayment != null && downPayment >= amount!!){
+                    loan_down_layout.error = resources.getString(R.string.loan_error_down_payment)
+                }
+            }
+            else -> {
+                canCalculate = true
+                loan_amount_layout.error = null
+                loan_term_layout.error = null
+                loan_interest_layout.error = null
+                loan_down_layout.error = null
+            }
+        }
+
+        if (canCalculate){
+            val result: Double
+            val totalPrice: Double
+            if(interest == 0.0){
+                result = (if (downPayment != null) ( amount!!-(downPayment)) else amount)!! / (term!! * 12)
+                totalPrice = 0.0
+            }else{
+                result = (if (downPayment != null) (amount!! -(downPayment)) else amount)!! * ((interest!! / (100)) / (12)) / (1 - Math.pow( 1 + ((interest / 100) / 12), -term!! *12))
+                totalPrice = 12 * term * result - (if (downPayment != null ) amount?.minus(downPayment)!! else amount!!)
+            }
+            loan_monthly.setText(String.format("%.2f",result), TextView.BufferType.EDITABLE)
+            loan_total.setText(String.format("%.2f",totalPrice), TextView.BufferType.EDITABLE)
+
+        }
+    }
+}
